7ed39e3b06583c0d3b5353af764ab7e032b056c8
Leonidas <leo.jbritos79@gmail.com>
2024-06-21 01:38:27 -0300

    Comienzo de web yeso

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './css/home.css'
+import './css/reset.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='contenedorPrincipal'>
+
+        <body className='bodyClass'>
+          <header className='headerClass'>
+            <img className='imagenMineria1' src="./public/img_minera_1.jpg" />
+            <div className='logoYNavbar'>
+              <img className='logoEmpresa' src="./public/logo.png" alt="" />
+              <ul className='navBar'>
+                <li className='botonNavBar'>PORTADA</li>
+                <li className='botonNavBar'>QUIENES SOMOS</li>
+                <li className='botonNavBar'>PRODUCTOS</li>
+                <li className='botonNavBar'>SERVICIOS</li>
+                <li className='botonNavBar'>CONTACTOS</li>
+              </ul>
+            </div>
+          </header>
+          <main className='mainClass'>
+            <div className='contenedorArticulos'>
+              <div className='quienesSomos'>
+                  <h3 className='H3QuienesSomos'>Yacimiento AGRO-INDUSTRIAL SULFACAL</h3> 
+                  <p className='pQuieneSomos'>Es una empresa dedicada a la producción de minerales para la
+                  industria y el agro.
+                  En la provincia de San Luís explota la Cantera El Portezuelo, yacimiento de “Yeso de alta ley”, ubicado en el paraje de Naranjo Esquino, próximo a la Localidad de Las Caleras y a 70 Km de la Ciudad de San Luís.
+                  Este yacimiento posee cinco niveles de yeso en forma estratificada, de rumbo medio norte - Sur e inclinación de 5 a 7º al este. De estos niveles se encuentran mapeados tres de ellos, denominados, de oeste a este 1, 2 y 4.
+                  La continuidad en superficie de estos niveles se puede verificar por más de tres mil metros
+                  Las Reservas de este yacimiento medidas al presente son las sector Norte, que ascienden a dos millones cien mil toneladas.
+                  Los niveles continúan al Sur, donde existen recursos por más de cuatro millones de toneladas adicionales.
+                </p>
+              </div>
+              <div className='muestraMineral'>
+                <div className='contenedorMuestraMineralImagen'>
+                  <h3 className='h3MuestraMineral'>Muestra de Mineral:</h3>
+                  <img className='muestraMineralImagen' src="./public/img_minera_2.jpg"alt="" />
+                </div>
+                  
+              </div>
+            </div>
+          </main>
+          <footer className='footerClass'> <p className='CopyrightP'> Copyright© 2024 - Leonidas Developer</p></footer>
+        </body>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
